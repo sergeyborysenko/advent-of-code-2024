5e7339196af18c7d2b4603ce61c4aca3560bd0f7
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-04 14:00:31 +0100

    [Day 1] Add trivial straightforward implementation

--- AdventOfCode2024/Day1/Program.cs
+++ AdventOfCode2024/Day1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "input.txt";
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        var lines = File.ReadAllLines(filePath);
+        lines.ToList().ForEach(line => {
+            var parts = line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
+            list1.Add(int.Parse(parts[0]));
+            list2.Add(int.Parse(parts[1]));
+        });
+        list1.Sort();
+        list2.Sort();
+        var sum = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            sum += Math.Abs(list1[i] - list2[i]);
+        }
+        Console.WriteLine(sum);
+    }
+}

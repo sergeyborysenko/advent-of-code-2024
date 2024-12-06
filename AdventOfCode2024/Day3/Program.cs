8cc4a1489a33816742032647b485a17ab3dda35a
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-06 09:47:34 +0100

    [Day 3] Add solution for the part 2

--- AdventOfCode2024/Day3/Program.cs
+++ AdventOfCode2024/Day3/Program.cs
@@ -3,7 +3,8 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 const string filePath = "input.txt";
-const string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+const string patternMul = @"mul\((\d{1,3}),(\d{1,3})\)";
+const string patternMulOrDos = @"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))";
 
 var reports = new List<List<int>>();
 var lines = File.ReadAllLines(filePath);
@@ -12,17 +13,52 @@ var stopwatch = Stopwatch.StartNew();
 var result = lines.Sum(line => CalculateInstructionsResult(line));
 stopwatch.Stop();
 Console.WriteLine($"CalculateInstructionsResult. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
-Console.ReadKey();
+
+stopwatch.Restart();
+result = CalculateInstructionsResultWithDos(lines);
+stopwatch.Stop();
+Console.WriteLine($"CalculateInstructionsResultWithDos. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
+Console.ReadKey();  
 
 long CalculateInstructionsResult(string line)
 {
     long result = 0;
-    var matches = Regex.Matches(line, pattern);
+    var matches = Regex.Matches(line, patternMul);
     foreach (Match match in matches)
     {
         int num1 = int.Parse(match.Groups[1].Value); 
         int num2 = int.Parse(match.Groups[2].Value);
         result += num1 * num2;
     }
+    return result;
+}
+
+long CalculateInstructionsResultWithDos(string[] lines)
+{
+    long result = 0;
+    bool doIt = true;
+    foreach (string line in lines)
+    {
+        var matches = Regex.Matches(line, patternMulOrDos);
+        foreach (Match match in matches)
+        {
+            if (match.Groups[0].Value.Equals("do()"))
+            {
+                doIt = true;
+                continue;
+            }
+            if (match.Groups[0].Value.Equals("don't()"))
+            {
+                doIt = false;
+                continue;
+            }
+            if (doIt)
+            {
+                int num1 = int.Parse(match.Groups[2].Value);
+                int num2 = int.Parse(match.Groups[3].Value);
+                result += num1 * num2;
+            }
+        }
+    }       
     return result;
 }

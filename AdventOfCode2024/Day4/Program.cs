2f3f3a52c153c41c70de9bb6f313dcdbc1a2447d
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-08 13:54:13 +0100

    [Day 4] Add solution for part 2

--- AdventOfCode2024/Day4/Program.cs
+++ AdventOfCode2024/Day4/Program.cs
@@ -2,8 +2,10 @@
 using System.Text.RegularExpressions;
 
 const string filePath = "input.txt";
+const int Width = 140;
+const int Height = 140;
 
-char[,] mat = new char[140, 140];
+char[,] mat = new char[Width, Height];
 char[] word = ['X', 'M', 'A', 'S'];
 
 var lines = File.ReadAllLines(filePath);
@@ -23,9 +25,15 @@ for (int i = 0; i < lines.Length; i++)
 }
 var stopwatch = Stopwatch.StartNew();
 var result = CalculatexXmasWords(mat);
-
 stopwatch.Stop();
 Console.WriteLine($"{nameof(CalculatexXmasWords)}. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
+
+stopwatch.Restart();
+result = CalculatexMasXPatterns(mat);
+stopwatch.Stop();
+Console.WriteLine($"{nameof(CalculatexXmasWords)}. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
+
+
 Console.ReadKey();
 
 int CalculatexXmasWords(char[,] mat)
@@ -35,7 +43,7 @@ int CalculatexXmasWords(char[,] mat)
     {
         for (int j = 0; j < mat.GetLength(1); j++)
         {
-            if (mat[i, j] == word[0])
+            if (mat[i, j] == word[0]/*'X'*/)
             {
                 result += FindPossibleXmasWord(mat, i, j);
             }
@@ -65,12 +73,42 @@ int FindXmasWordByDirection(char[,] mat, int i, int j, int charInd, int di, int 
         return 1;
     }
 
-    if (i + di >= 0 && i + di < 140 && j + dj >= 0 && j + dj < 140)
+    if (i + di >= 0 && i + di < Width && j + dj >= 0 && j + dj < Height)
     {
         if (mat[i + di, j + dj] == word[charInd])
         {
             return FindXmasWordByDirection(mat, i + di, j + dj, charInd+1, di, dj);
         }
+    }
+    return 0;
+}
+
+int CalculatexMasXPatterns(char[,] mat)
+{
+    int result = 0;
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            if (mat[i, j] == word[2]/*'A'*/)
+            {
+                result += IsСenterOfMasXPattern(mat, i, j);
+            }
+        }
+    }
+    return result;
+}
+
+int IsСenterOfMasXPattern(char[,] mat, int i, int j)
+{
+    if (i == 0 || j == 0 || i == Width - 1 || j == Height -1)
+        return 0;
+    if ((mat[i-1,j-1] == word[1] && mat[i+1, j+1] == word[3]) ||
+        (mat[i - 1, j - 1] == word[3] && mat[i + 1, j + 1] == word[1]))
+    {
+        if ((mat[i - 1, j + 1] == word[1] && mat[i + 1, j - 1] == word[3]) ||
+        (mat[i - 1, j + 1] == word[3] && mat[i + 1, j - 1] == word[1]))
+            return 1;
     }
     return 0;
 }

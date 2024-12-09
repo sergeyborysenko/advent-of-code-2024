275e4b1349439041660a7ece05566f52934b63ac
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-09 12:30:34 +0100

    [Day 5] Add solution for part 2

--- AdventOfCode2024/Day5/Program.cs
+++ AdventOfCode2024/Day5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Tracing;
 
 const string filePath = "input.txt";
 
@@ -11,11 +12,19 @@ var result = CalculatexSum(orderRules, updates);
 stopwatch.Stop();
 Console.WriteLine($"{nameof(CalculatexSum)}. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
 
+stopwatch.Restart();
+result = CalculatexSumForCorrectedUpdates(orderRules, updates.Where(update => !IsCorrectlyOrdered(update, orderRules)).ToArray());
+stopwatch.Stop();
+Console.WriteLine($"{nameof(CalculatexSumForCorrectedUpdates)}. Result: {result}; Execution Time: {stopwatch.ElapsedTicks} ticks");
+
 int CalculatexSum(Dictionary<int, HashSet<int>> orderRules, int[][] updates)
 {
     return updates.Where(update => IsCorrectlyOrdered(update, orderRules)).Sum(update => update[update.Length / 2]);
 }
-
+int CalculatexSumForCorrectedUpdates(Dictionary<int, HashSet<int>> orderRules, int[][] updates)
+{
+    return updates.Select(update => CorrectUpdate(update, orderRules)).Sum(corrected => corrected[corrected.Length / 2]);
+}
 bool IsCorrectlyOrdered(int[] update, Dictionary<int, HashSet<int>> rules)
 {
     var visited = new HashSet<int>();
@@ -37,6 +46,33 @@ bool IsCorrectlyOrdered(int[] update, Dictionary<int, HashSet<int>> rules)
         }
     }
     return true;
+}
+
+int[] CorrectUpdate(int[] update, Dictionary<int, HashSet<int>> rules)
+{
+    var visited = new HashSet<int>();
+    var corrected = new List<int>();
+    foreach (var pageNumber in update)
+    {
+        if (!rules.ContainsKey(pageNumber))
+        {
+            corrected.Add(pageNumber);
+            visited.Add(pageNumber);
+            continue;
+        }
+        var rule = rules[pageNumber];
+        if (rule.Any(r => visited.Contains(r)))
+        {
+            var index = corrected.FindIndex(c => rule.Contains(c));
+            corrected.Insert(index, pageNumber);
+        }
+        else
+        {
+            corrected.Add(pageNumber);
+            visited.Add(pageNumber);
+        }
+    }
+    return [.. corrected];
 }
 
 (Dictionary<int, HashSet<int>>, int[][]) ParseInput(string[] lines)

e3aab7c57b368dd4e74f0ea5e4f32ce87913a77f
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-11 13:36:58 +0100

    [Day 8] Add solution for part 2

--- AdventOfCode2024/Day8/Program.cs
+++ AdventOfCode2024/Day8/Program.cs
@@ -1,6 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Reflection;
+﻿using System.Diagnostics;
+using System.Numerics;
 
 const string filePath = "input.txt";
 const int Width = 50;
@@ -13,9 +12,15 @@ var coordinates = ParseAntennasCoordinates(lines);
 
 var stopwatch = Stopwatch.StartNew();
 var result = GetDistinctAntinodesCount([.. coordinates.Values], mat);
-
 stopwatch.Stop();
 Console.WriteLine($"{nameof(GetDistinctAntinodesCount)}. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
+
+ClearMatrix(mat);
+
+stopwatch.Restart();
+result = GetDistinctResonantAntinodesCount([.. coordinates.Values], mat);
+stopwatch.Stop();
+Console.WriteLine($"{nameof(GetDistinctResonantAntinodesCount)}. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
 
 Dictionary<char, List<(int, int)>> ParseAntennasCoordinates(string[] lines)
 {
@@ -46,29 +51,63 @@ int GetDistinctAntinodesCount(List<List<(int, int)>> coordinatesLists, int[,] ma
         foreach (var (ant1, ant2) in pairs)
         {
             var (diffX, diffY) = (ant2.X - ant1.X, ant2.Y - ant1.Y);
-            if (IsNewAntinode(ref mat, ant1.X - diffX, ant1.Y - diffY))
+            if (IsNewAntinode(mat, ant1.X - diffX, ant1.Y - diffY))
                 result++;
-            if (IsNewAntinode(ref mat, ant2.X + diffX, ant2.Y + diffY))
+            if (IsNewAntinode(mat, ant2.X + diffX, ant2.Y + diffY))
                 result++;
         }
     }
     return result;
 }
-
-bool IsNewAntinode(ref int[,] mat, int x, int y)
+int GetDistinctResonantAntinodesCount(List<List<(int, int)>> coordinatesLists, int[,] mat)
 {
-    if (x >= 0 && x < Width && y >= 0 && y < Height)
+    var result = 0;
+    foreach (var coordinatesList in coordinatesLists)
     {
-        if (mat[x, y] != 1)
+        var pairs = GetAllPairs(coordinatesList);
+        foreach (var (ant1, ant2) in pairs)
         {
-            mat[x, y] = 1;
-            return true;
+            var (diffX, diffY) = (ant2.X - ant1.X, ant2.Y - ant1.Y);
+            var gcd = (int)BigInteger.GreatestCommonDivisor(diffX, diffY);
+            diffX /= gcd;
+            diffY /= gcd;
+            var node = ant1;
+            // Tracing the line one direction from ant1
+            while(IsWithinBounds(node.X, node.Y))
+            {
+                if (IsNewAntinode(mat, node.X, node.Y))
+                    result++;
+                node = (node.X - diffX, node.Y - diffY);
+            }
+            node = ant1;
+            // Tracing the line another direction from ant1
+            while (IsWithinBounds(node.X, node.Y))
+            {
+                if (IsNewAntinode(mat, node.X, node.Y))
+                    result++;
+                node = (node.X + diffX, node.Y + diffY);
+            }
         }
+    }
+    return result;
+}
+
+
+bool IsWithinBounds(int x, int y)
+{
+    return x >= 0 && x < Width && y >= 0 && y < Height;
+}
+bool IsNewAntinode(int[,] mat, int x, int y)
+{
+    if (IsWithinBounds(x, y) && mat[x,y] != 1)
+    {
+        mat[x, y] = 1;
+        return true;
     }
     return false;
 }
 
-List<((int X, int Y) Ant1, (int X, int Y) Ant2)> GetAllPairs(List<(int, int)> coordinatesList)
+List<((int X, int Y) Ant1, (int X, int Y) Ant2)> GetAllPairs(List<(int X, int Y)> coordinatesList)
 {
     var pairs = new List<((int X, int Y) Ant1, (int X, int Y) Ant2)>();
     for (int i = 0; i < coordinatesList.Count; i++)
@@ -79,4 +118,15 @@ List<((int X, int Y) Ant1, (int X, int Y) Ant2)> GetAllPairs(List<(int, int)> co
         }
     }
     return pairs;
+}
+
+void ClearMatrix(int[,] mat)
+{
+    for (int i = 0; i < Width; i++)
+    {
+        for (int j = 0; j < Height; j++)
+        {
+            mat[i, j] = 0;
+        }
+    }
 }

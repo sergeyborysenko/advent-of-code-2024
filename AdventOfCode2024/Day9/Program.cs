ed31984c08c00dde79f12b0091ad300cdf4d62cd
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-12 20:40:52 +0100

    [Day 9] Add solution for part 2

--- AdventOfCode2024/Day9/Program.cs
+++ AdventOfCode2024/Day9/Program.cs
@@ -8,15 +8,20 @@ const string filePath = "input.txt";
 var lines = File.ReadAllLines(filePath);
 
 var stopwatch = Stopwatch.StartNew();
-var result = GetFilesystemChecksum(lines.First());
+var result = GetFilesystemChecksum(lines.First(), true);
 stopwatch.Stop();
-Console.WriteLine($"{nameof(GetFilesystemChecksum)}. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
+Console.WriteLine($"Optimize by moving blocks. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
+
+stopwatch.Restart();
+result = GetFilesystemChecksum(lines.First(), false);
+stopwatch.Stop();
+Console.WriteLine($"Optimize by moving files. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
 Console.ReadKey();
 
-long GetFilesystemChecksum(string line)
+long GetFilesystemChecksum(string line, bool moveByBlocks)
 {
     var fileSystem = ParseDiskmap(line);
-    var optimizedFileSystem = OptimizeFileSystem(fileSystem);
+    var optimizedFileSystem = moveByBlocks ? OptimizeByBlocks(fileSystem) : OptimizeByWholeFiles(fileSystem);
     return CalculateChecksum(optimizedFileSystem);
 }
 
@@ -32,7 +37,7 @@ long CalculateChecksum(List<int> optimizedFileSystem)
     return checksum;
 }
 
-List<int> OptimizeFileSystem(List<int> fileSystem)
+List<int> OptimizeByBlocks(List<int> fileSystem)
 {
     var leftInd = 0;
     var rightInd = fileSystem.Count - 1;
@@ -49,6 +54,76 @@ List<int> OptimizeFileSystem(List<int> fileSystem)
         }
     }
     return fileSystem;
+}
+
+List<int> OptimizeByWholeFiles(List<int> fileSystem)
+{
+    var rightInd = fileSystem.Count - 1;
+    while (rightInd > 0)
+    {
+        rightInd = SeekNextFileWithHighestId(fileSystem, rightInd);
+        var fileSize = GetNextFileSize(fileSystem, rightInd);
+        var freeSpaceIndex = SeekSuitedFreeSpaceIndex(fileSystem, rightInd, fileSize);
+        if (freeSpaceIndex != -1)
+        {
+            for (int i = 0; i < fileSize; i++)
+            {
+                fileSystem[freeSpaceIndex + i] = fileSystem[rightInd - i];
+                fileSystem[rightInd - i] = -1;
+            }
+        }
+        rightInd -= fileSize;
+    }
+    return fileSystem;
+}
+int SeekSuitedFreeSpaceIndex(List<int> fileSystem, int rightIndex, int fileSize)
+{
+    var leftIndex = 0;
+    while (leftIndex < rightIndex)
+    {
+        var nextFreeSpaceIndex = SeekNextFreeSpace(fileSystem, leftIndex);
+        var freeSpaceSize = GetFreeSpaceSize(fileSystem, nextFreeSpaceIndex);
+        if (freeSpaceSize >= fileSize && nextFreeSpaceIndex < rightIndex)
+        {
+            return nextFreeSpaceIndex;
+        }
+        leftIndex = nextFreeSpaceIndex + freeSpaceSize;
+    }
+    return -1;
+}
+int SeekNextFreeSpace(List<int> fileSystem, int index)
+{
+    while (index < fileSystem.Count && fileSystem[index] != -1)
+        index++;
+    return index;
+}
+int SeekNextFileWithHighestId(List<int> fileSystem, int index)
+{
+    while (index < fileSystem.Count && fileSystem[index] == -1)
+        index--;
+    return index;
+}
+
+int GetFreeSpaceSize(List<int> fileSystem, int index)
+{
+    var freeSpace = 0;
+    while (index < fileSystem.Count && fileSystem[index] == -1)
+    {
+        freeSpace++;
+        index++;
+    }
+    return freeSpace;
+}
+int GetNextFileSize(List<int> fileSystem, int index)
+{
+    var fileSize = 0;
+    var fileId = fileSystem[index];
+    while (index >= 0 && fileSystem[index] == fileId)
+    {
+        fileSize++;
+        index--;
+    }
+    return fileSize;
 }
 
 List<int> ParseDiskmap(string line)

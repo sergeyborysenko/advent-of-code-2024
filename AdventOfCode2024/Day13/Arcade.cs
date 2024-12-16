b8c3b8bd826d544f5809be0fb223f946a7b0b98b
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-16 13:58:38 +0100

    [Day 13] Add solution for part 1

---
+++ AdventOfCode2024/Day13/Arcade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    internal class Arcade
+    {
+        public const int A_Tokens = 3;
+        public const int B_Tokens = 1;
+
+        public int SpentTokens { get; private set; } = 0;
+        public (int X, int Y) CurrentPosition { get; private set; } = (0, 0);
+
+        public (int X, int Y) A { get; set; }
+        public (int X, int Y) B { get; set; }
+        public (int X, int Y) Prize { get; set; }
+
+        public int CalculateNumberOfTokens()
+        {
+            decimal upArg = Prize.X * B.Y - Prize.Y * B.X;
+            decimal downArg = A.X * B.Y - A.Y * B.X;
+            var a = Math.Round(upArg / downArg);
+            var b = Math.Round(Prize.X - a * A.X) / B.X;
+            
+            if (a > 100 || b > 100 || a * A.X + b * B.X != Prize.X || a * A.Y + b * B.Y != Prize.Y)
+                return 0;
+            
+            return A_Tokens * (int)a + B_Tokens * (int)b;
+        }
+    }
+}
---
+++ AdventOfCode2024/Day13/Program.cs
@@ -0,0 +1,80 @@
+﻿using Day13;
+using System.Diagnostics;
+
+const string filePath = "input.txt";
+const int Width = 140;
+const int Height = 140;
+
+var mat = new char[Width, Height];
+
+var lines = File.ReadAllLines(filePath);
+var arcades = ParseInput(lines);
+
+
+var stopwatch = Stopwatch.StartNew();
+var result = CountMinAmountTokens(arcades);
+
+stopwatch.Stop();
+Console.WriteLine($"{nameof(CountMinAmountTokens)} by score. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
+Console.ReadKey();
+
+int CountMinAmountTokens(List<Arcade> arcades)
+{
+    var minAmountTokens = 0;
+    foreach (var arcade in arcades)
+    {
+        minAmountTokens += arcade.CalculateNumberOfTokens();
+    }
+    return minAmountTokens;
+}
+
+List<Arcade> ParseInput(string[] lines)
+{
+    var arcades = new List<Arcade>();
+    Arcade? currentArcade = null;
+
+    foreach (var line in lines)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            if (currentArcade != null)
+            {
+                arcades.Add(currentArcade);
+                currentArcade = null;
+            }
+            continue;
+        }
+
+        if (line.StartsWith("Button A:"))
+        {
+            var coordinates = line.Substring(9).Split(',');
+            var x = int.Parse(coordinates[0].Split('+')[1]);
+            var y = int.Parse(coordinates[1].Split('+')[1]);
+            currentArcade ??= new Arcade();
+            currentArcade.A = (x, y);
+        }
+        else if (line.StartsWith("Button B:"))
+        {
+            var coordinates = line.Substring(9).Split(',');
+            var x = int.Parse(coordinates[0].Split('+')[1]);
+            var y = int.Parse(coordinates[1].Split('+')[1]);
+            currentArcade ??= new Arcade();
+            currentArcade.B = (x, y);
+        }
+        else if (line.StartsWith("Prize:"))
+        {
+            var coordinates = line.Substring(6).Split(',');
+            var x = int.Parse(coordinates[0].Split('=')[1]);
+            var y = int.Parse(coordinates[1].Split('=')[1]);
+            currentArcade ??= new Arcade();
+            currentArcade.Prize = (x, y);
+        }
+    }
+
+    if (currentArcade != null)
+    {
+        arcades.Add(currentArcade);
+    }
+
+    return arcades;
+}

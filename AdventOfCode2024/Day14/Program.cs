8996e30a8b03473f37d2163cb146bc466b56e358
Sergey Borysenko <sergey.borysenko@elementlogic.se>
2024-12-18 22:43:26 +0100

    [Day 14] Add solution for part 1

---
+++ AdventOfCode2024/Day14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+const string filePath = "input.txt";
+const int Width = 101;
+const int Height = 103;
+
+var mat = new int[Height, Width];
+
+
+var lines = File.ReadAllLines(filePath);
+var robots = ParseInput(lines);
+
+var stopwatch = Stopwatch.StartNew();
+var result = CountSafetyFactor(robots, 100);
+stopwatch.Stop();
+Console.WriteLine($"{nameof(CountSafetyFactor)}. Result: {result}; Execution Time: {stopwatch.ElapsedMilliseconds} miliseconds");
+
+IEnumerable<Robot?> ParseInput(string[] lines)
+{
+    foreach(var line in lines)
+    {
+        yield return Robot.TryParse(line, out var robot) ? robot : null;
+    }
+}
+
+long CountSafetyFactor(IEnumerable<Robot?> robots, int seconds)
+{
+    foreach (var robot in robots)
+    {
+        if (robot == null)
+        {
+            continue;
+        }
+
+        robot.Move(Height, Width, seconds);
+        mat[robot.Position.X, robot.Position.Y]++;
+    }
+    var kv1 = CountNumberOfRobots(mat, 0, Height/2, 0, Width / 2);
+    var kv2 = CountNumberOfRobots(mat, Height / 2 + 1, Height, 0, Width / 2);
+    var kv3 = CountNumberOfRobots(mat, 0, Height/2, Width / 2 + 1, Width );
+    var kv4 = CountNumberOfRobots(mat, Height / 2 + 1, Height, Width / 2 + 1, Width);
+
+    return kv1 * kv2 * kv3 * kv4;
+}
+
+int CountNumberOfRobots(int[,] mat, int startX, int endX, int startY, int endY)
+{
+    var count = 0;
+    for (int x = startX; x < endX; x++)
+    {
+        for (int y = startY; y < endY; y++)
+        {
+            count += mat[x, y];
+        }
+    }
+    return count;
+}
---
+++ AdventOfCode2024/Day14/Robot.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Robot
+{
+    private const string inputPattern = @"p=(?<py>-?\d+),(?<px>-?\d+) v=(?<vy>-?\d+),(?<vx>-?\d+)";
+
+    public (int X, int Y) Position { get; set; }
+    public (int X, int Y) Vector { get; set; }
+
+    public void Move(int height, int width, int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            var potentialPosition = (Position.X + Vector.X, Position.Y + Vector.Y);
+            Position = IsOutOfBounds(potentialPosition, height, width) ?
+                GetTeleportedPosition(potentialPosition, height, width)
+                : potentialPosition;
+        }
+    }
+
+    private static (int X, int Y) GetTeleportedPosition((int X, int Y) potentialPosition, int height, int width)
+    {
+        if (potentialPosition.X < 0)
+        {
+            potentialPosition.X = height + potentialPosition.X;
+        }
+        if (potentialPosition.X >= height)
+        {
+            potentialPosition.X -= height;
+        }
+        if (potentialPosition.Y < 0)
+        {
+            potentialPosition.Y = width + potentialPosition.Y;
+        }
+        if (potentialPosition.Y >= width)
+        {
+            potentialPosition.Y -= width;
+        }
+        return potentialPosition;
+    }
+
+    private static bool IsOutOfBounds((int X, int Y) potentialPosition, int height, int width) => potentialPosition.X < 0 || potentialPosition.X >= height || potentialPosition.Y < 0 || potentialPosition.Y >= width;
+
+    public static bool TryParse(string input, out Robot? robot)
+    {
+        try
+        {
+            Regex regex = new(inputPattern);
+            var match = regex.Match(input);
+            if (!match.Success)
+            {
+                robot = null;
+                return false;
+            }
+            int posX = int.Parse(match.Groups["px"].Value);
+            int posY = int.Parse(match.Groups["py"].Value);
+            int vecX = int.Parse(match.Groups["vx"].Value);
+            int vecY = int.Parse(match.Groups["vy"].Value);
+
+            robot = new Robot
+            {
+                Position = (posX, posY),
+                Vector = (vecX, vecY)
+            };
+            return true;
+        }
+        catch
+        {
+            robot = null;
+            return false;
+        }
+    }
+}
